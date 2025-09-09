90ece260e8837870df40e949adf583061034a08a
ChaitanyaBhatt147 <bhattchaitanya43@gmail.com>
2025-09-09 16:53:23 +0530

    use of scanner class

--- src/in/co/rays/oop/TestPerson.java
+++ src/in/co/rays/oop/TestPerson.java
@@ -2,17 +2,23 @@ package in.co.rays.oop;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class TestPerson {
 	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Person p1 = new Person();
-		p1.setName("Chaitanya Bhatt");
-		p1.setDob(sdf.parse("2001-07-14"));
-		p1.setAddress("Niranjanpur, Indore");
+		System.out.println("Enter your name.");
+		p1.setName(sc.nextLine());
+		System.out.println("Enter your DOB in YYYY-MM-DD formate.");
+		p1.setDob(sdf.parse(sc.nextLine()));
+		System.out.println("Enter your address.");
+		p1.setAddress(sc.nextLine());
 		
 		System.out.println("Name: "+ p1.getName());
 		System.out.println("DOB: "+ p1.getDob());
 		System.out.println("Address: "+p1.getAddress());
+		sc.close();
 	}
 }
